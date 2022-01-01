a7b2a23b28c42debd7633c2bd4c8ee258dc1cd47
vivjk94 <96954665+vivjk94@users.noreply.github.com>
2022-01-02 00:12:10 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,5 @@
+print("Welcome to your Bank \n LOGIN DETAILS")
+Name=input("Hello, please enter your Name\n")
+Byear=int(input("Please enter your birthyear"))
+Age=str(2021-Byear)
+print("Hello, Mr. "+Name+"You are "+Age+" Thank you for logging in."+ Name[0:4])
